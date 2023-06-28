74a682130aa17af31afbcb817f91bc0be18a23a7
Meet Thummar <thummarmeet15@gmail.com>
2023-06-28 23:46:44 +0530

    Commited just for fun, changed button styles and added functionality to count laps

--- StopWatch.py
+++ StopWatch.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 running = False
 hours, minutes, seconds = 0, 0, 0
+laps = []
+currentTime = ""
 
 def start():
     global running
@@ -19,10 +21,17 @@ def reset():
     global running
     if running:
         stopwatch_label.after_cancel(update_time)
+        lap_label.after_cancel(update_time)
         running = False
     global hours, minutes, seconds
     hours, minutes, seconds = 0, 0, 0
+    laps.clear()
     stopwatch_label.config(text = '00:00:00')
+    lap_label.config(text = '')
+
+def lap():
+    laps.append(currentTime)
+    lap_label.config(text="\n".join(map(str, laps)))
 
 def update():
     global hours, minutes, seconds
@@ -38,27 +47,38 @@ def update():
     hours_string = f'{hours}' if hours > 9 else f'0{hours}'
     minutes_string = f'{minutes}' if minutes > 9 else f'0{minutes}'
     seconds_string = f'{seconds}' if seconds > 9 else f'0{seconds}'
+
+    global currentTime 
+    currentTime = hours_string + ':' + minutes_string + ':' + seconds_string
     
-    stopwatch_label.config(text = hours_string + ':' + minutes_string + ':' + seconds_string)
+    stopwatch_label.config(text = currentTime)
     
     global update_time
     update_time = stopwatch_label.after(1000, update)
 
 
 root = tk.Tk()
-root.geometry('485x220')
 root.title('Stopwatch')
 
-stopwatch_label = tk.Label(text = '00:00:00', font = ('Arial', 80))
+stopwatch_label = tk.Label(text = '00:00:00', font = ('Arial', 50))
 stopwatch_label.pack()
 
-start_button = tk.Button(text = 'Start', height=5, width=7, font=('Arial', 20), command = start)
+
+lap_label = tk.Label(text="", justify="left", font = ('Arial', 20), pady=4)
+lap_label.pack(side=tk.BOTTOM)
+
+button_style = {"background": "#bfbfbf", "foreground": "#000000", "activebackground": "#bfbfbf", "activeforeground": "#000000"}
+
+start_button = tk.Button(root, text="Start", command=start, **button_style, height=1, width=7, font=('Arial', 20),)
 start_button.pack(side = tk.LEFT)
-pause_button = tk.Button(text = 'Pause', height=5, width=7, font=('Arial', 20), command = pause)
+pause_button = tk.Button(root, text="Pause", command=pause, **button_style, height=1, width=7, font=('Arial', 20),)
 pause_button.pack(side = tk.LEFT)
-reset_button = tk.Button(text = 'Reset', height=5, width=7, font=('Arial', 20), command = reset)
+reset_button =  tk.Button(root, text="Lap", command=lap, **button_style, height=1, width=7, font=('Arial', 20),)
 reset_button.pack(side = tk.LEFT)
-quit_button = tk.Button(text = 'Quit', height=5, width=7, font=('Arial', 20), command = root.quit)
+reset_button =  tk.Button(root, text="Reset", command=reset, **button_style, height=1, width=7, font=('Arial', 20),)
+reset_button.pack(side = tk.LEFT)
+quit_button =  tk.Button(root, text="Quit", command=root.quit, **button_style, height=1, width=7, font=('Arial', 20),)
 quit_button.pack(side = tk.LEFT)
+
 
 root.mainloop()
